3477aff50344114ce813ce1415198e9a6e114ed3
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-11-22 10:34:34 +0000

    agregando modal y nuevas funciones

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -51,9 +51,28 @@ export const Home = () => {
 		</div>
 
 		<div className="col-lg-1 col-md-6 p-3">
-			<button className="sideButtons" onClick={() => botonParaEliminarContacto(contact.id)}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+			<button type="button" className="btn sideButtons" data-bs-toggle="modal" data-bs-target="#exampleModal">
+			<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
   				<path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-			</svg></button>
+			</svg>
+			</button>
+			<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Seguro que quieres eliminar a este usuario?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No.</button>
+        <button onClick={() => botonParaEliminarContacto(contact.id)} type="button" class="btn btn-primary">Sí, eliminalo</button>
+      </div>
+    </div>
+  </div>
+</div>
 		
  		 </div>
 
